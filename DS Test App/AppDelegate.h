194a2c7a854159445160aa937ec0f354d82ba12d
Grygorii Tarashchuk <gtarashuk@gmail.com>
2017-03-23 01:00:16 +0200

    Initial Commit

---
+++ DS Test App/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DS Test App
+//
+//  Created by Grigory Tarashuk on 3/23/17.
+//  Copyright © 2017 Test Name. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DS Test App/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DS Test App
+//
+//  Created by Grigory Tarashuk on 3/23/17.
+//  Copyright © 2017 Test Name. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
